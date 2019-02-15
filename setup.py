58c3f46158a20515ec8e583c9e2d07f030ec8158
Serial Lab <slab@serial-lab.com>
2019-02-14 17:01:56 -0700

    Bump version: 2.0.3 → 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='list_based_flavorpack',
-    version='2.0.3',
+    version='2.0.4',
     packages=find_packages(),
     include_package_data=True,
     url='https://gitlab.com/serial-lab/list_based_flavorpack',
